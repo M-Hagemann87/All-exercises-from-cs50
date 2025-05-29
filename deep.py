3a4b526748b939afd778ac969efc46245f01c908
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-05-29 11:57:31 -0300

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -2,7 +2,6 @@ def main():
     answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
     answer = answer.strip()
     answer = answer.lower()
-    print(answer)
     match answer:
         case "42"|"forty two"|"forty-two":
             print("Yes")
