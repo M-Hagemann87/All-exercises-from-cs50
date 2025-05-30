5db331d3a835b574161f5cba8e05a8e120cf3b48
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-05-30 16:43:09 -0300

    automated commit by check50 [check50=True]

--- coke.py
+++ coke.py
@@ -5,15 +5,16 @@ def main():
     logic(n)
 
 def logic(n):
+    coin  = input("Insert Coin: ")
     while n >= 0:
-        coin  = input("Insert Coin: ")
+        coin  = input()
         coin = int(coin)
         if coin == 30:
             coin = 0
         coin = int(coin)
         n = n  - coin
         if  n > 0:
-            print("Amount Due: ", n)
+            print("Amount Due: ",n)
         elif n  <= 0:
             change = abs(n)
             print("Change Owed:", change)
