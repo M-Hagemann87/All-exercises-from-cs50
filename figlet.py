ff10e96da098d29a3ab7aac4cb433e258622608f
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-04 16:39:01 -0300

    automated commit by check50 [check50=True]

--- figlet.py
+++ figlet.py
@@ -4,21 +4,28 @@ from pyfiglet import Figlet
 
 figlet = Figlet()
 #Random
-if len(sys.argv) < 2:
-        figlet = Figlet()
-        y = figlet.getFonts()
-        x = random.choice(y)
-        set_font = Figlet(font=x)
-        text_input =  input("Input: ")
-        print(set_font.renderText(text_input))
-        exit()
+try:
+    if len(sys.argv) < 2:
+            figlet = Figlet()
+            y = figlet.getFonts()
+            x = random.choice(y)
+            set_font = Figlet(font=x)
+            text_input =  input("Input: ")
+            print(set_font.renderText(text_input))
 
-elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"]:
-    choice_font = sys.argv[2]
-    print(choice_font)
-    figlet.setFont(font=choice_font)
+    elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"]:
+        choice_font = sys.argv[2]
+        if choice_font not in figlet.getFonts():
+               print("Invalid usage")
+               sys.exit(1)
 
+        figlet.setFont(font=choice_font)
+        text_input = input("Input: ")
+        print(figlet.renderText(text_input))
 
-    text_input = input("Input: ")
-    print(figlet.renderText(text_input))
-    exit()
+    else:
+          print("Invalid usage")
+
+except:
+        sys.exit(1)
+
