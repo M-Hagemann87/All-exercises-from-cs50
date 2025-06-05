d08b09a4da7113191a582f7af8b14989fdd4bc0a
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-05 13:30:22 -0300

    automated commit by check50 [check50=True]

--- professor.py
+++ professor.py
@@ -1,49 +1,51 @@
 import random
 
-tries = int()
-score = int()
-questions = int()
-while True:
-        if questions == 10:
-            print("Score: ", score)
-            exit(0)
-        if questions < 10:
-            tries = 0
-            while True:
-                    try:
-                        level = int(input("Level: "))
-                        if level == 1:
-                            n1 = 0
-                            n2 = 9
-                            break
-                        elif level == 2:
-                            n1 = 10
-                            n2 = 99
-                            break
-                        elif level == 3:
-                            n1 = 100
-                            n2 = 999
-                            break
-                    except ValueError:
-                        continue
-            number1 = random.randint(n1, n2)
-            number2 = random.randint(n1, n2)
-            result = number1 + number2
-            while True:
-                try:
-                    in_calc = int(input(f"{number1} + {number2} = "))
-                    if tries <= 1:
-                        if result == in_calc:
-                            score += 1
-                            questions += 1
-                            tries += 1
-                            break
-                        else:
-                            print("EEE")
-                            tries += 1
-                    else:
-                        questions += 1
-                        print(f"{number1} + {number2} = {result}")
-                        break
-                except ValueError:
-                    continue
+def main():
+    level = get_level()
+    score = 0
+    questions = 0
+
+    while questions < 10:
+        number1, number2 = generate_integer(level)
+        result = number1 + number2
+        correct = ask_question(number1, number2, result)
+        if correct:
+            score += 1
+        questions += 1
+
+    print("Score:", score)
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level (1-Easy, 2-Medium, 3-Hard): "))
+            if level in [1, 2, 3]:
+                return level
+        except ValueError:
+            pass  # Ignore and reprompt
+
+def generate_integer(level):
+    if level == 1:
+        return random.randint(0, 9), random.randint(0, 9)
+    elif level == 2:
+        return random.randint(10, 99), random.randint(10, 99)
+    else:
+        return random.randint(100, 999), random.randint(100, 999)
+
+def ask_question(number1, number2, result):
+    tries = 0
+    while tries < 3:
+        try:
+            answer = int(input(f"{number1} + {number2} = "))
+            if answer == result:
+                return True
+            else:
+                print("EEE")
+        except ValueError:
+            print("EEE")
+        tries += 1
+    print(f"{number1} + {number2} = {result}")
+    return False
+
+if __name__ == "__main__":
+    main()
