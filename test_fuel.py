d5e6d1e6c27565792cb7e87a188d913e1d44395a
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-06 17:17:39 -0300

    automated commit by check50 [check50=True]

--- test_fuel.py
+++ test_fuel.py
@@ -13,13 +13,13 @@ def test_gauge():
 
     assert gauge(1) == "E"
     assert gauge(100) == "F"
-    assert gauge(75) == "75"
-    assert gauge(50) == "50"
-    assert gauge(25) == "25"
+    assert gauge(75) == "75%"
+    assert gauge(50) == "50%"
+    assert gauge(25) == "25%"
     assert gauge(99) == "F"
 
 def test_invalid():
-    
+
     with pytest.raises(ZeroDivisionError):
         convert("1/0")
 
