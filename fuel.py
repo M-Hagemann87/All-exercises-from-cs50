2673c998702fb8cac480c8c5957443ce770298aa
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-06 16:10:42 -0300

    automated commit by check50 [check50=True]

--- fuel.py
+++ fuel.py
@@ -1,16 +1,15 @@
 def main():
-
-    call_1 = input("Fraction: ")
-    call_2 = convert(call_1)
-    p_result = gauge(call_2)
     try:
+        call_1 = input("Fraction: ")
+        call_2 = convert(call_1)
+        p_result = gauge(call_2)
         if p_result.isdigit():
             print(p_result,"%", sep="")
         else:
             print(p_result)
 
     except ValueError:
-        print("ValueError")
+        exit(1)
     except ZeroDivisionError:
         print("ZeroDivisionError")
 
