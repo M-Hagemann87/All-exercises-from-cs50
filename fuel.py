9bd9cfe0096564873aaa313617ab28df31413e91
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-06 16:06:51 -0300

    automated commit by check50 [check50=True]

--- fuel.py
+++ fuel.py
@@ -1,22 +1,50 @@
-while True:
-    try:
-        fraction = input("Fraction: ").strip()
-        x, y = map(int, fraction.split("/"))
-        solution = (x / y)*100
-        solution = round(solution)
-        solution = int(solution)
+def main():
 
-        if solution > 100:
-            continue
-        elif solution > 98:
-            print("F")
-        elif solution < 2:
-            print("E")
-        elif 1 < solution < 99:
-            print(solution, "%", sep="")
-        break
+    call_1 = input("Fraction: ")
+    call_2 = convert(call_1)
+    p_result = gauge(call_2)
+    try:
+        if p_result.isdigit():
+            print(p_result,"%", sep="")
+        else:
+            print(p_result)
 
     except ValueError:
         print("ValueError")
     except ZeroDivisionError:
         print("ZeroDivisionError")
+
+
+def convert(fraction):
+    while True:
+        fraction = fraction.strip()
+        x, y = map(int, fraction.split("/"))
+        solution = (x / y) * 100
+        solution = round(solution)
+        fraction = int(solution)
+        try:
+            if fraction > 100:
+                False
+            else:
+                return fraction
+        except ValueError:
+            print("ValueError")
+        except ZeroDivisionError:
+            print("ZeroDivisionError")
+
+def gauge(percentage):
+
+        if percentage > 98:
+            percentage = str(percentage)
+            percentage = "F"
+            return percentage
+        elif percentage < 2:
+            percentage = str(percentage)
+            percentage = "E"
+            return percentage
+        elif 1 < percentage < 99:
+            percentage = str(percentage)
+            return percentage
+
+if __name__ =="__main__":
+    main()
