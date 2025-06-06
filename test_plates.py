621654b1728afd2026b4de11fc98c2f2cac925cb
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-06 13:52:29 -0300

    automated commit by check50 [check50=True]

--- test_plates.py
+++ test_plates.py
@@ -9,22 +9,30 @@ def test_plate1():
     assert is_valid("NAR2A") == False
     assert is_valid("NARA3A") == False
     assert is_valid("NARAA4") == True
-
-def test_plate2():
-    assert is_valid("a") == False
-    assert is_valid("aa") == False
-    assert is_valid("A") == False
-    assert is_valid("AAAAAAA") == False
-
-def test_plate3():
     assert is_valid("AA") == True
     assert is_valid("AAA") == True
     assert is_valid("AAAA") == True
     assert is_valid("AAAAA") == True
     assert is_valid("AAAAAA") == True
-
-def test_plate4():
+    assert is_valid("a") == False
+    assert is_valid("aa") == False
+    assert is_valid("A") == False
+    assert is_valid("AAAAAAA") == False
+    assert is_valid("AAAAAAAA") == False
+    assert is_valid("AAAAAAAAA") == False
+    assert is_valid("AAAAAAAAAA") == False
     assert is_valid("AA.123") == False
     assert is_valid("A.A") == False
     assert is_valid("AAAAA,") == False
     assert is_valid("AA!AA") == False
+
+def test_plate2():
+     assert is_valid("AA!AA") == False
+
+
+
+def test_plate3():
+    assert is_valid("AA!AA") == False
+
+def test_plate4():
+    assert is_valid("AAAAA,") == False
