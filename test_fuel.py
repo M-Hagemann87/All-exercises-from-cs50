d0d0df19307286f47701e717851fd52a495d91dd
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-09 08:33:39 -0300

    automated commit by check50 [check50=True]

--- test_fuel.py
+++ test_fuel.py
@@ -13,8 +13,8 @@ def test_gauge():
 
     assert gauge(1) == "E"
     assert gauge(100) == "F"
-    assert gauge(75) == "75"
-    assert gauge(50) == "50"
-    assert gauge(25) == "25"
+    assert gauge(75) == "75%"
+    assert gauge(50) == "50%"
+    assert gauge(25) == "25%"
     assert gauge(99) == "F"
 
