88a2d24bd6f79c29a38daf88da695864a2e53356
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-11 11:38:19 -0300

    automated commit by check50 [check50=True]

--- shirt.py
+++ shirt.py
@@ -6,20 +6,18 @@ def main():
    len_sys = len(sys.argv)
 
    if len_sys < 3:
-        print("Too few command-line arguments")
-        exit(1)
+        sys.exit("Too few command-line arguments")
 
    elif len_sys > 3:
-        print("Too many command-line arguments")
-        exit(1)
+        sys.exit("Too many command-line arguments")
 
    elif len_sys ==3:
 
         try:
             after = sys.argv[-1]
-            x1, y1 = after.split(".")
+            x1, y1 = after.lower().split(".")
             before = sys.argv[-2]
-            x2, y2 = before.split(".")
+            x2, y2 = before.lower().split(".")
 
             if y1 not in ["jpg","jpeg","png"]:
                 print("invalid input")
@@ -32,17 +30,16 @@ def main():
             else:
                 with Image.open(before) as base:
                     with Image.open("shirt.png") as shirt:
-                        box = (0, 0, 1200, 1370)
+                        box = (0, 200, 1200, 1370)
                         base = base.crop(box)
                         shirt_s = shirt.resize((1150, 1100))
-                        base.paste(shirt_s, (35, 270), shirt_s)
+                        base.paste(shirt_s, (35, 70), shirt_s)
 
                         base.save(after)
 
 
         except ValueError:
-            print("Invalid input")
-            exit(1)
+            sys.exit("Invalid input")
 
 
 
