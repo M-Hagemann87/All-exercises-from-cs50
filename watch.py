9c5c4d7ad008210484f341c864520cd0edfcb949
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-12 13:48:03 -0300

    automated commit by check50 [check50=True]

--- watch.py
+++ watch.py
@@ -6,7 +6,7 @@ def main():
 
 def parse(s):
 
-    matches = re.search(r"^(<iframe)?([a-zA-Z0-9_ =\"]*)\"?https?://(www\.)?youtube\.com/embed/([a-zA-Z0-9_]+).+?$", s)
+    matches = re.search(r"^(<iframe)?([a-zA-Z0-9_ =\"]*)\"?https?://(www\.)?youtube\.com/embed/([a-zA-Z0-9_]+)([a-zA-Z0-9_ =\"]*)(></iframe>)?$", s)
     if matches:
         print("https://youtu.be/" + matches.group(4))
         exit(0)
