79ddea36811f663be5bd130b8778c28f80af985c
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-12 16:12:58 -0300

    automated commit by check50 [check50=True]

--- test_working.py
+++ test_working.py
@@ -3,17 +3,17 @@ from working import convert
 
 def test_s():
 
-    assert convert("12 AM to 1 PM") == "12:00 to 13:00"
-    assert convert("1 AM to 12 PM") == "01:00 to 00:00"
-    assert convert("7PM to 7AM") == "19:00 to 07:00"
-    assert convert("3PMto3AM") == "15:00 to 03:00"
-    assert convert("2 PMto2 AM") == "14:00 to 02:00"
+    assert convert("12 AM to 1 PM") == "00:00 to 13:00"
+    assert convert("1 AM to 12 PM") == "01:00 to 12:00"
+    assert convert("7 PM to 7 AM") == "19:00 to 07:00"
+    assert convert("3 PM to 3 AM") == "15:00 to 03:00"
+    assert convert("2 PM to2 AM") == "14:00 to 02:00"
     assert convert("9:13 AM to 5:14 PM") == "09:13 to 17:14"
     assert convert("2:21 AM to 2:21 PM") == "02:21 to 14:21"
     assert convert("1:59 AM to 1:59 PM") == "01:59 to 13:59"
-    assert convert("12:20 AM to 12:20 PM") == "12:20 to 00:20"
-    assert convert("11:37 PMto11:11 AM") == "23:37 to 11:11"
-    assert convert("7:37PMto2:37AM") == "19:37 to 02:37"
+    assert convert("12:20 AM to 12:20 PM") == "00:20 to 12:20"
+    assert convert("11:37 PM to 11:11 AM") == "23:37 to 11:11"
+    assert convert("7:37 PM to 2:37 AM") == "19:37 to 02:37"
 
 
 def test_wrongtime():
