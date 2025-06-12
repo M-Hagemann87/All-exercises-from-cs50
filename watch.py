42486d24f85f997631fb1fc6a51cb31b116abd72
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-12 12:57:39 -0300

    automated commit by check50 [check50=True]

---
+++ watch.py
@@ -0,0 +1,15 @@
+import re
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+
+    matches = re.search(r"\"?https?://(www\.)?youtube.com/embed/([a-zA-Z0-9_]+).+?$", s)
+    if matches:
+        print("https://youtu.be/" + matches.group(2))
+        exit(0)
+
+if __name__ == "__main__":
+    main()
