06453bbd9cd831c5b35a601b065b4a82204dce6f
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-26 17:09:38 -0300

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -1,3 +1,3 @@
 SELECT COUNT("first_name") FROM "players"
 WHERE ("bats" = 'R' AND "trows" = 'L')
-OR ("bats" = 'L' AND "trows" = 'R');
+AND ("bats" = 'L' AND "trows" = 'R');
--- 8.sql
+++ 8.sql
@@ -1,2 +1,2 @@
 SELECT ROUND(AVG("height"), 2) AS "Average Height", ROUND(AVG("weigth"), 2) AS "Average Weight" FROM "players"
-WHERE "debuted" <= date('2000-01-01');
+WHERE "debuted" BETWEEN date('2000-01-01') AND ('2025-12-31');
