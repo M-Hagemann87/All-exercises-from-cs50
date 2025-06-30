209d3f441876e564eee908084b7e05868d341efd
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-30 09:53:24 -0300

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -3,4 +3,6 @@ FROM "districts"
 LEFT JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
 LEFT JOIN "schools" ON "schools"."district_id" = "districts"."id"
 LEFT JOIN "graduation_rates" ON "graduation_rates"."school_id" = "schools"."id"
-ORDER BY "expenditures"."per_pupil_expenditure" DESC, "schools"."name";
+WHERE "graduation_rates"."graduated" != 'NULL' and "expenditures"."per_pupil_expenditure" != 'NULL'
+ORDER BY "expenditures"."per_pupil_expenditure" DESC, "schools"."name"
+LIMIT 20;
