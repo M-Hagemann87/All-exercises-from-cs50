b340f8650b27cc543344b5d865056d571dee97b8
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-30 10:48:12 -0300

    automated commit by check50 [check50=True]

--- 10.sql
+++ 10.sql
@@ -4,4 +4,3 @@ WHERE "districts"."type" = 'Public School District'
 ORDER BY "expenditures"."per_pupil_expenditure" DESC
 LIMIT 10;
 
---WHERE "schools"."type" = 'Public School' AND "schools"."state" = 'MA'
