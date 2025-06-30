66a2ec7950c9a0bc5cc609a936ffd83fd64d07dd
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-30 15:52:23 -0300

    automated commit by check50 [check50=True]

---
+++ 2.sql
@@ -0,0 +1,10 @@
+SELECT "year", "salary" FROM "salaries"
+WHERE "player_id" = '15726'
+GROUP BY "year"
+ORDER BY "year" DESC;
+
+=====
+--SELECT * FROM "players"
+--WHERE "last_name" = 'Ripken';
+--Cal Ripken Jr.
+--id =  15726
