7bfdb55110c4ab8eba6adbd8c7a1f7dd719f7d16
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-30 16:10:59 -0300

    automated commit by check50 [check50=True]

--- 2.sql
+++ 2.sql
@@ -1,9 +1,10 @@
 SELECT "year", "salary" FROM "salaries"
-WHERE "player_id" = '15726'
+JOIN "players" ON "players"."id" = "salaries"."player_id"
+WHERE "players"."first_name" = 'Cal'
+AND "players"."last_name" = 'Ripken'
 GROUP BY "year"
 ORDER BY "year" DESC;
 
-=====
 --SELECT * FROM "players"
 --WHERE "last_name" = 'Ripken';
 --Cal Ripken Jr.
