f46e12cd1940da7c254f5df455f75068437c1c68
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-06-30 16:52:00 -0300

    automated commit by check50 [check50=True]

--- 5.sql
+++ 5.sql
@@ -1,5 +1,6 @@
-SELECT "teams"."name" FROM "salaries"
-JOIN "teams" ON "teams"."id" = "salaries"."team_id"
-JOIN "players" ON "players"."id" = "salaries"."player_id"
-WHERE "players"."first_name" = 'Satchel'
-AND "players"."last_name" = 'Paige';
+SELECT DISTINCT teams.name
+FROM salaries
+JOIN teams ON teams.id = salaries.team_id
+JOIN players ON players.id = salaries.player_id
+WHERE players.first_name = 'Satchel'
+  AND players.last_name = 'Paige';
--- 6.sql
+++ 6.sql
@@ -1,5 +1,6 @@
-SELECT "performances"."AB" AS "total hits", "teams"."name" FROM  "performances"
+SELECT "performances"."H" AS "total hits", "teams"."name" FROM  "performances"
 FULL JOIN "teams" ON "teams"."id" = "performances"."team_id"
 WHERE "performances"."year" = '2001'
 ORDER BY "total hits" DESC
 LIMIT 5;
+
