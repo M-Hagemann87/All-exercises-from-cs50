69cac82847f275f1acf866a3424ce06fa28cdb34
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-01 11:17:32 -0300

    automated commit by check50 [check50=True]

--- 5.sql
+++ 5.sql
@@ -1,6 +1,5 @@
-SELECT DISTINCT teams.name
-FROM salaries
-JOIN teams ON teams.id = salaries.team_id
-JOIN players ON players.id = salaries.player_id
-WHERE players.first_name = 'Satchel'
-  AND players.last_name = 'Paige';
+SELECT "teams"."name" FROM "salaries"
+JOIN "teams" ON "teams"."id" = "salaries"."team_id"
+JOIN "players" ON "players"."id" = "salaries"."player_id"
+WHERE "players"."first_name" = 'Satchel'
+AND "players"."last_name" = 'Paige';
--- 8.sql
+++ 8.sql
@@ -1,5 +1,14 @@
 SELECT "salary" FROM "salaries"
 JOIN "performances" ON "performances"."player_id" = "salaries"."player_id"
 WHERE "performances"."year" = '2001'
-ORDER BY "H" DESC
+ORDER BY "H" DESC;
 LIMIT 1;
+
+SELECT * FROM "salaries"
+WHERE "player_id" = 17288;
+LIMIT 50;
+
+
+SELECT * FROM "performances"
+ORDER BY "H" DESC
+LIMIT 50;
