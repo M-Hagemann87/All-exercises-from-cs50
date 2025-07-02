0d72d33269dfcca404df3ab75a5e3d0e04f18a65
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-02 13:37:41 -0300

    automated commit by check50 [check50=True]

--- 9.sql
+++ 9.sql
@@ -1,6 +1,6 @@
-SELECT "teams"."name", ROUND(AVG("salary"), 2) AS "average salary" FROM "salaries"
+SELECT "teams"."name", ROUND(AVG("salaries"."salary"), 2) AS "average salary" FROM "salaries"
 JOIN "teams" ON "teams"."id" = "salaries"."team_id"
-GROUP BY "teams"."name"
+GROUP BY "teams"."id"
 ORDER BY "average salary"
 LIMIT 5;
 
