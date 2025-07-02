ae89596ccb7d971cc45ceedfcb0de138971663a9
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-02 16:51:28 -0300

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -4,7 +4,8 @@ LEFT JOIN "players" ON "players"."id" = "salaries"."player_id"
 RIGHT JOIN "performances" ON "performances"."player_id" = "salaries"."player_id"
 WHERE "performances"."H" != '0'
 AND "salaries"."year" = '2001'
-GROUP BY "salaries"."player_id"
+AND "performances"."year" = '2001'
+GROUP BY "salaries"."player_id", "performances"."player_id"
 ORDER BY "dollars per hit", "players"."first_name", "players"."last_name"
 LIMIT 10;
 
