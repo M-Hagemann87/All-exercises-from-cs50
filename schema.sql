59cdef555d02d5a94f0465a31263a1fcd0c895a3
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-03 13:50:54 -0300

    automated commit by check50 [check50=True]

--- schema.sql
+++ schema.sql
@@ -7,29 +7,28 @@ CREATE TABlE "passengers"(
 
 CREATE TABLE "check_ins" (
     "date" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "flight_id",
-    "passenger_last_name",
+    "flight_id" INTEGER,
+    "passenger_last_name" TEXT,
     FOREIGN KEY "flight_id" REFERENCES "flights"("id"),
     FOREIGN KEY "passenger_last_name" REFERENCES "passengers"("last_name"),
 );
 
 CREATE TABLE "airlines" (
-    "id",
-    "name" NOT NULL,
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
     "concourse" TEXT NOT NULL CHECK ("concourse" IN ('A','B','C','D','E','F','T')),
     PRIMARY KEY ("id")
 );
 
 CREATE TABLE "flights"(
-    "id",
-    "passenger_last_name",
-    "passenger_first_name",
+    "id" INTEGER,
+    "passenger_last_name" TEXT,
     "flight_number" INTEGER,
-    "airlines_id",
+    "airlines_id"  INTEGER,
     "departing" TEXT NOT NULL CHECK ("departing" IN ('ATL','BOS')),
-    "heading",
-    "departure_time",
-    "arrival_time",
+    "heading" TEXT,
+    "departure_time" NUMERIC,
+    "arrival_time" NUMERIC,
     PRIMARY KEY ("id"),
     FOREIGN KEY "passenger_last_name" REFERENCES "passengers"("last_name"),
     FOREIGN  KEY "airlines_id" REFERENCES "airlines"("id")
