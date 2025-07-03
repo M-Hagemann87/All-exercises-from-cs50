800b54652ae3e637f02d1b2585928529e8d9c6db
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-03 14:25:17 -0300

    automated commit by check50 [check50=True]

--- schema.sql
+++ schema.sql
@@ -1,36 +1,38 @@
-CREATE TABlE "passengers"(
-    "first_name" TEXT,
-    "last_name" TEXT,
-    "age"  INTEGER,
-    "check_id" INTEGER,
-    FOREIGN KEY "check_id" REFERENCES "check"("id")
-);
-
-CREATE TABLE "check" (
-    "id" INTEGER,
-    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "flight_id" INTEGER,
-    PRIMARY KEY ("id")
-    FOREIGN KEY "flight_id" REFERENCES "flights"("id"),
+CREATE TABLE "passengers" (
+    "id" INTEGER PRIMARY KEY,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL
 );
 
 CREATE TABLE "airlines" (
-    "id" INTEGER,
-    "name" TEXT NOT NULL,
-    "concourse" TEXT NOT NULL CHECK ("concourse" IN ('A','B','C','D','E','F','T')),
-    PRIMARY KEY ("id")
+    "id" INTEGER PRIMARY KEY,
+    "name" TEXT NOT NULL
 );
 
-CREATE TABLE "flights"(
-    "id" INTEGER,
-    "flight_number" INTEGER,
-    "airlines_id"  INTEGER,
+-- Airline can operate in multiple concourses
+CREATE TABLE "airline_concourses" (
+    "airline_id" INTEGER,
+    "concourse" TEXT CHECK ("concourse" IN ('A','B','C','D','E','F','T')),
+    FOREIGN KEY ("airline_id") REFERENCES "airlines"("id")
+);
+
+CREATE TABLE "flights" (
+    "id" INTEGER PRIMARY KEY,
+    "flight_number" INTEGER NOT NULL,
+    "airlines_id" INTEGER NOT NULL,
     "departing" TEXT NOT NULL CHECK ("departing" IN ('ATL','BOS')),
-    "heading" TEXT,
-    "departure_time" NUMERIC,
-    "arrival_time" NUMERIC,
-    "check_id" INTEGER,
-    PRIMARY KEY ("id"),
-    FOREIGN KEY "check_id" REFERENCES "check"("id")
-    FOREIGN  KEY "airlines_id" REFERENCES "airlines"("id")
+    "heading" TEXT NOT NULL,
+    "departure_time" NUMERIC NOT NULL,
+    "arrival_time" NUMERIC NOT NULL,
+    FOREIGN KEY ("airlines_id") REFERENCES "airlines"("id")
+);
+
+CREATE TABLE "check_ins" (
+    "id" INTEGER PRIMARY KEY,
+    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "flight_id" INTEGER NOT NULL,
+    "passenger_id" INTEGER NOT NULL,
+    FOREIGN KEY ("flight_id") REFERENCES "flights"("id"),
+    FOREIGN KEY ("passenger_id") REFERENCES "passengers"("id")
 );
