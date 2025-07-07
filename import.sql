f9689b781e6637924ca7be8fd59afd88b00cd2a1
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-07 09:41:24 -0300

    automated commit by check50 [check50=True]

--- import.sql
+++ import.sql
@@ -1,4 +1,4 @@
---1
+--1 Create final and temp tables
 CREATE TABLE "meteorites"(
     "name" TEXT,
     "id" INTEGER NOT NULL,
@@ -23,34 +23,38 @@ CREATE TABLE "meteorites_temp"(
     PRIMARY KEY ("id")
 );
 
---2
+--2 Import data
 .import --csv --skip 1 meteorites.csv meteorites_temp
 
---3 (1)
-UPDATE "meteorites_temp" SET "mass" = NULL
-WHERE "mass" = '';
+--3 Replace empty values with NULL
+UPDATE "meteorites_temp" SET "mass" = NULL WHERE "mass" = '';
+UPDATE "meteorites_temp" SET "year" = NULL WHERE "year" = '';
+UPDATE "meteorites_temp" SET "lat" = NULL WHERE "lat" = '';
+UPDATE "meteorites_temp" SET "long" = NULL WHERE "long" = '';
 
-UPDATE "meteorites_temp" SET "year" = NULL
-WHERE "year" = '';
+--4 Round decimal values
+UPDATE "meteorites_temp" SET "mass" = ROUND("mass", 2);
+UPDATE "meteorites_temp" SET "lat" = ROUND("lat", 2);
+UPDATE "meteorites_temp" SET "long" = ROUND("long", 2);
 
-UPDATE "meteorites_temp" SET "lat" = NULL
-WHERE "lat" = '';
-
-UPDATE "meteorites_temp" SET "long" = NULL
-WHERE "long" = '';
---4 (2)
-UPDATE "meteorites_temp" SET "lat" = ROUND("lat",2);
-
-UPDATE "meteorites_temp" SET "long" = ROUND("long",2);
-
-UPDATE "meteorites_temp" SET "mass" = ROUND("mass",2);
---5 (3)
+--5 Remove Relict rows
 DELETE FROM "meteorites_temp" WHERE "nametype" = 'Relict';
---6
 
----
+--6 Insert into final table with reordered and renumbered IDs
+WITH ordered AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY "year", "name") AS "id",
+        "name",
+        "nametype",
+        "mass",
+        "year",
+        "lat",
+        "long"
+    FROM "meteorites_temp"
+)
 INSERT INTO "meteorites" ("id", "name", "nametype", "mass", "year", "lat", "long")
-SELECT "id", "name", "nametype", "mass", "year", "lat", "long" FROM "meteorites_temp"
-ORDER BY "year", "name";
+SELECT "id", "name", "nametype", "mass", "year", "lat", "long"
+FROM ordered;
 
+--7 Drop temp table
 DROP TABLE IF EXISTS "meteorites_temp";
