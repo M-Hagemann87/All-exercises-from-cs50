3f39b41cf425723f464fbc8ceadf292e3de73672
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-07 11:12:16 -0300

    automated commit by check50 [check50=True]

--- import.sql
+++ import.sql
@@ -28,11 +28,17 @@ CREATE TABLE "meteorites_temp"(
 .import --csv --skip 1 meteorites.csv meteorites_temp
 
 --3 (1)
-UPDATE "meteorites_temp" SET
-    "mass" = NULLIF("mass", ''),
-    "year" = NULLIF("year", ''),
-    "lat" = NULLIF("lat", ''),
-    "long" = NULLIF("long", '');
+UPDATE "meteorites_temp" SET "mass" = NULL
+WHERE "mass" = '';
+
+UPDATE "meteorites_temp" SET "year" = NULL
+WHERE "year" = '';
+
+UPDATE "meteorites_temp" SET "lat" = NULL
+WHERE "lat" = '';
+
+UPDATE "meteorites_temp" SET "long" = NULL
+WHERE "long" = '';
 
 --4 (2)
 UPDATE "meteorites_temp" SET "lat" = ROUND("lat",2);
