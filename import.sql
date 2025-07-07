263e03baba19c44c364a0085db55748501238d4e
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-07 11:01:39 -0300

    automated commit by check50 [check50=True]

--- import.sql
+++ import.sql
@@ -1,4 +1,17 @@
-CREATE TABLE "meteorites_temp" (
+--1
+CREATE TABLE "meteorites"(
+    "name" TEXT,
+    "id" INTEGER NOT NULL,
+    "class" TEXT,
+    "mass" REAL,
+    "discovery" TEXT,
+    "year" INTEGER,
+    "lat" REAL,
+    "long" REAL,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "meteorites_temp"(
     "name" TEXT,
     "id" INTEGER NOT NULL,
     "nametype" TEXT,
@@ -8,39 +21,37 @@ CREATE TABLE "meteorites_temp" (
     "year" INTEGER,
     "lat" REAL,
     "long" REAL,
-    PRIMARY KEY("id")
+    PRIMARY KEY ("id")
 );
 
-CREATE TABLE "meteorites" (
-    "id" INTEGER NOT NULL,
-    "name" TEXT,
-    "class" TEXT,
-    "mass" REAL,
-    "discovery" TEXT,
-    "year" INTEGER,
-    "lat" REAL,
-    "long" REAL,
-    PRIMARY KEY("id")
-);
-
+--2
 .import --csv --skip 1 meteorites.csv meteorites_temp
 
-UPDATE "meteorites_temp" SET
-    "mass" = NULLIF("mass", ''),
-    "year" = NULLIF("year", ''),
-    "lat" = NULLIF("lat", ''),
-    "long" = NULLIF("long", '');
+--3 (1)
+UPDATE "meteorites_temp" SET "mass" = NULL
+WHERE "mass" = '';
 
-UPDATE "meteorites_temp"
-SET "mass" = ROUND("mass", 2),
-    "lat" = ROUND("lat", 2),
-    "long" = ROUND("long", 2);
+UPDATE "meteorites_temp" SET "year" = NULL
+WHERE "year" = '';
 
+UPDATE "meteorites_temp" SET "lat" = NULL
+WHERE "lat" = '';
+
+UPDATE "meteorites_temp" SET "long" = NULL
+WHERE "long" = '';
+--4 (2)
+UPDATE "meteorites_temp" SET "lat" = ROUND("lat",2);
+
+UPDATE "meteorites_temp" SET "long" = ROUND("long",2);
+
+UPDATE "meteorites_temp" SET "mass" = ROUND("mass",2);
+--5 (3)
 DELETE FROM "meteorites_temp" WHERE "nametype" = 'Relict';
+--6
 
-INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
-SELECT "name", "class", "mass", "discovery", "year", "lat", "long"
-FROM "meteorites_temp"
+---
+INSERT INTO "meteorites" ("id", "name", "nametype", "mass", "year", "lat", "long")
+SELECT "id", "name", "nametype", "mass", "year", "lat", "long" FROM "meteorites_temp"
 ORDER BY "year", "name";
 
-DROP TABLE "meteorites_temp";
+DROP TABLE IF EXISTS "meteorites_temp";
