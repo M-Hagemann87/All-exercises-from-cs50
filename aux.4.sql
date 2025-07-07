16b325dad31ece0729582a7581cf601ff209f650
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-07 10:35:42 -0300

    automated commit by check50 [check50=True]

--- aux.4.sql
+++
@@ -1,9 +0,0 @@
-
-SELECT * FROM "meteorites_temp"
-WHERE "nametype" = 'Relict'
-LIMIT  10;
----
-DELETE FROM "meteorites_temp" WHERE "nametype" = 'Relict';
-
-
-DELETE FROM "collections" WHERE "title" = 'Spring outing';
