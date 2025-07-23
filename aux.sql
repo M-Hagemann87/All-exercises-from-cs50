4c70e26e3992a3a496f623471a6feb13a2b1a246
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-23 09:36:49 -0300

    automated commit by check50 [check50=True]

--- aux.sql
+++ aux.sql
@@ -1,3 +1,38 @@
+#####4
+SELECT "username" FROM "users"
+WHERE "id" IN (
+    SELECT "to_user_id" ,COUNT("to_user_id") FROM "messages"
+    GROUP BY "to_user_id"
+    ORDER BY "to_user_id" DESC
+    )
+LIMIT 1;
+
+
+SELECT "to_user_id" ,COUNT("to_user_id") FROM "messages"
+    GROUP BY "to_user_id"
+    ORDER BY "to_user_id" DESC
+    Limit 25;
+
+SELECT "to_user_id" ,COUNT("to_user_id") FROM "messages"
+    GROUP BY "to_user_id"
+    ORDER BY "to_user_id" DESC
+    Limit 25;
+
+
+
+SELECT "username" FROM "users"
+WHERE "id" = "2500"
+LIMIT 10;
+
+
+SELECT * FROM "users"
+WHERE "username" = "impressivecommitment918"
+LIMIT 1;
+
+
+
+
+#####3
 SELECT "users"."id" FROM "users"
 JOIN "messages" ON "messages"."from_user_id" = "users"."id"
 WHERE "from_user_id" = (
@@ -32,6 +67,8 @@ WHERE "from_user_id" IN (
     SELECT "id" FROM "users"
     WHERE "username" = "creativewisdom377"
 )
+GROUP BY "to_user_id"
+ORDER BY "from_user_id" DESC
 LIMIT 3;
 
 ######
