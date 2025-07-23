fd26d0ba84bb64a3b5a6474cc0bb54ac98ad67e7
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-23 11:08:00 -0300

    automated commit by check50 [check50=True]

--- 4.sql
+++ 4.sql
@@ -1,9 +1,8 @@
-SELECT u.username
-FROM users u
-JOIN (
-    SELECT to_user_id
-    FROM messages
-    GROUP BY to_user_id
+SELECT "username" FROM "users"
+WHERE "id" IN (
+    SELECT "to_user_id" FROM "messages"
+    GROUP BY "to_user_id"
     ORDER BY COUNT(*) DESC
     LIMIT 1
-) AS top_user ON u.id = top_user.to_user_id;
+    )
+;
