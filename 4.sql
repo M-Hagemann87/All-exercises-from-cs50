fac5fe75389e63852673f679cedc1b32eba70298
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-23 10:51:47 -0300

    automated commit by check50 [check50=True]

--- 4.sql
+++ 4.sql
@@ -1,7 +1,6 @@
 SELECT "username" FROM "users"
 WHERE "id" IN (
     SELECT "to_user_id"."messages" FROM "messages"
-    GROUP BY "to_user_id"."messages"
     ORDER BY COUNT("id")."messages" DESC
     )
 LIMIT 1;
