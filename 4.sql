264b9dd6906119e1a25187128c12af0c1617136e
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-07-23 10:49:50 -0300

    automated commit by check50 [check50=True]

--- 4.sql
+++ 4.sql
@@ -1,7 +1,7 @@
 SELECT "username" FROM "users"
 WHERE "id" IN (
-    SELECT "to_user_id" FROM "messages"
-    GROUP BY "to_user_id"
-    ORDER BY COUNT("picture") DESC
+    SELECT "to_user_id"."messages" FROM "messages"
+    GROUP BY "to_user_id"."messages"
+    ORDER BY COUNT("id")."messages" DESC
     )
 LIMIT 1;
