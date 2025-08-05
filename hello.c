372a5486a6ce1de7df2b35f5a755d4bf35783f11
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-08-05 10:28:23 -0300

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
 // Program to say hello to a prompted name/user
 
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main (void)
+int main(void)
 
 {
     string user = get_string("What is your name?: \n");
