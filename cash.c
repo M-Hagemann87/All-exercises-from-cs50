ed4a25737883ee8de5dbe2e836710ea7e6be7f4d
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-08-05 14:55:11 -0300

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -1,7 +1,7 @@
 // Cash exercise, subtract the highest value coin
 
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -9,35 +9,34 @@ int main(void)
     int count;
     do
     {
-    coin = get_int("Chage owed: ");
-    count = 0;
+        coin = get_int("Chage owed: ");
+        count = 0;
     }
     while (0 > coin);
-
 
     // Subtract 25
     while (coin >= 25)
     {
-    coin = coin - 25;
-    count++;
+        coin = coin - 25;
+        count++;
     }
     // Subtract 10
     while (coin >= 10)
     {
-    coin = coin - 10;
-    count++;
+        coin = coin - 10;
+        count++;
     }
     // Subtract 5
     while (coin >= 5)
     {
-    coin = coin - 5;
-    count++;
+        coin = coin - 5;
+        count++;
     }
     // Subtract 1
     while (coin >= 1)
     {
-    coin = coin - 1;
-    count++;
+        coin = coin - 1;
+        count++;
     }
     printf("%i", count);
     printf("\n");
