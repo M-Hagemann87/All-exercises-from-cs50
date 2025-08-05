97b87b768d98430bb5e1e018d10799fc64b31f65
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-08-05 13:36:47 -0300

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int bricks);
+
+int main(void)
+{
+    // Prompt the user for the pyramid's height
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n > 9 || n < 1);
+
+    // Print a pyramid of that height
+    for (int i = 0; i < n; i++)
+    {
+        // Print row of bricks
+        print_row(i + 1);
+    }
+}
+
+void print_row(int bricks)
+{
+    int cal = 9 - bricks;
+    for (int j = cal; j > 0; j--)
+    {
+        printf(" ");
+    }
+    for (int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
