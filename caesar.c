47d851fabd0dfa16270e1b8cc2c6284ba41af9d5
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-08-07 15:59:10 -0300

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,16 +1,59 @@
-#include <cs50.h>
-#include <ctype.h>
-#include <stdio.h>
+// Program to find the grade of a specific text;
 
-int main(void)
+#include <ctype.h> // is digit --> yes need to use it
+#include <cs50.h> //  get_string, etc..
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // For exit(0).
+
+int rotate(string text);
+
+int main(int argc, string argv[])
+////////////ok===>
 {
-    char c = get_char("Input: ");
-    if (isdigit(c))
+    string number = argv[1];
+
+    if (argv[2] != NULL)
     {
-        printf("Your input is a digit.\n");
+        printf("Usage: %s key\n", argv[0]);
+        exit(1);
     }
-    else
+    for (int i = 0; number[i] != '\0'; i++)
     {
-        printf("Your input is not a digit.\n");
+        char c = number[i];
+        if (isdigit(number[i]))
+        {
+            continue;
+        }
+        else
+        {
+            printf("Usage: %s key\n", argv[0]);
+            exit(1);
+        }
     }
+////////////ok===>
+    int new_number = atoi(number);
+
+    string text = get_string("plaintext: ");
+
+    printf("ciphertext: ");
+
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        char new_text = text[i];
+
+        if (isalpha(new_text))
+        {
+            char ABab = isupper(new_text) ? 'A' : 'a';
+            char rotated = (new_text - ABab + new_number) % 26 + ABab;
+            printf("%c", rotated);
+        }
+        else
+        {
+            printf("%c", new_text);
+        }
+    }
+
+    printf("\n");
 }
