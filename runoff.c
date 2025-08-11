855d395f91291ef6e6556bc43f2aa6f4a4ef9a18
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-08-11 13:27:47 -0300

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -189,9 +189,13 @@ int find_min(void) /// stoped here!!
             {
                 min_vote = candidates[i].votes;
             }
+        }
+        
+         for (int i = 0; i < candidate_count; i++)
+        {
             if (candidates[i].votes == min_vote)
             {
-                 if (candidates[i].eliminated == false)
+                 if (candidates[i].eliminated == true)
                     {
                     return min_vote;
                     }
