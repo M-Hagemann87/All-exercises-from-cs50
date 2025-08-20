8cf14c6cfe7ef8694a8a34089b19a74adab28434
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-08-20 12:01:33 -0300

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,13 +1,18 @@
-height = int(input("Height: "))
+height = 0
 try:
     while height > 8 or height < 1:
         height = int(input("Height: "))
         continue
 except ValueError:
-    test = 0
+    height = 0
+
+start_space =  8 - height
 
 for i in range (height):
-    spaces = 7 - i
+    spaces = 7 - start_space - i
+
+
+
     print(" "*spaces, end="")
     print("#"*(i+1), end="")
     print("  ", end="")
