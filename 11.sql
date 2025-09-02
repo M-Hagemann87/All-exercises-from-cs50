fa49634beeaa5bbb567098e0444de4c4c49b8ad6
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-09-02 14:55:19 -0300

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -3,6 +3,5 @@ WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
 AND ratings.movie_id = movies.id
 AND people.name = 'Chadwick Boseman'
-GROUP BY movies.title
 ORDER  BY ratings.rating DESC
 LIMIT 5;
