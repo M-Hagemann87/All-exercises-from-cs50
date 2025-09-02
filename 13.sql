52915670e25b4b5c63cfa9b674901b25465168c0
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-09-02 14:50:25 -0300

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -1,7 +1,7 @@
 SELECT people.name FROM movies, people, stars
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
---AND NOT name = 'Kevin Bacon'
+AND NOT name = 'Kevin Bacon'
 AND stars.movie_id = (SELECT movies.id FROM movies, people, stars
     WHERE movies.id = stars.movie_id
     AND people.id = stars.person_id
