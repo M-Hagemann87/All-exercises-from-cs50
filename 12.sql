522aae8d01ce4ea276bb3534363da3dfdbd32063
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-09-02 15:54:09 -0300

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,5 +1,6 @@
 SELECT title FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
-AND people.name = 'Bradley Cooper'
-AND people.name = 'Jennifer Lawrence';
+AND people.name IN ('Jennifer Lawrence', 'Bradley Cooper')
+GROUP BY title
+HAVING COUNT(DISTINCT people.name) = 2;
--- 13.sql
+++ 13.sql
@@ -2,7 +2,7 @@ SELECT people.name FROM movies, people, stars
 WHERE movies.id = stars.movie_id
 AND people.id = stars.person_id
 AND NOT name = 'Kevin Bacon'
-AND stars.movie_id = (SELECT movies.id FROM movies, people, stars
+AND stars.movie_id IN (SELECT movies.id FROM movies, people, stars
     WHERE movies.id = stars.movie_id
     AND people.id = stars.person_id
     AND people.name = 'Kevin Bacon'
