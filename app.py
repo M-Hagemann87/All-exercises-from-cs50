a4cc00ddada7d0b0318909a4ec2353697f79b3ff
M-Hagemann87 <M-Hagemann87@users.noreply.github.com>
2025-09-15 13:48:01 -0300

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -6,19 +6,13 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
-
-# Configure application
 app = Flask(__name__)
-
-# Custom filter
 app.jinja_env.filters["usd"] = usd
 
-# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
 
@@ -32,8 +26,8 @@ def after_request(response):
 
     return response
 
-
 @app.route("/")
+
 @login_required
 def index():
     """Show portfolio of stocks"""
@@ -118,7 +112,6 @@ def buy():
 
 
 
-
 @app.route("/history")
 @login_required
 def history():
@@ -137,37 +130,23 @@ def history():
 def login():
     """Log user in"""
 
-    # Forget any user_id
     session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
-
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for username
         rows = db.execute(
             "SELECT * FROM users WHERE username = ?", request.form.get("username")
         )
 
-        # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(
             rows[0]["hash"], request.form.get("password")
         ):
             return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
         return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
@@ -175,10 +154,7 @@ def login():
 @app.route("/logout")
 def logout():
     """Log user out"""
-    # Forget any user_id
     session.clear()
-
-    # Redirect user to login form
     return redirect("/")
 
 
@@ -212,8 +188,6 @@ def register():
             return apology("must provide password")
         if password != confirmation:
             return apology("passwords must match")
-
-        # Create user with hashed password
         hash_ = generate_password_hash(password)
 
         try:
@@ -224,7 +198,6 @@ def register():
         except Exception:
             return apology("username taken")
 
-        # Log in
         session["user_id"] = new_id
         flash("Registered!")
         return redirect("/")
